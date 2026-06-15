433169a826af896b0d0c947a7b9d42f13be83c89
MIS_FB <mis2@frozenbottle.in>
2026-06-15 15:46:41 +0530

    Update item_level.py

--- item_level.py
+++ item_level.py
@@ -1000,6 +1000,142 @@ lw_sales = lw_df.copy()
 print("✅ Sales DataFrames Created")
 
 # =========================================================
+
+# FIX ITEM METRIC VALUES
+
+# =========================================================
+
+column_mapping = {
+"quantity": "item_quantity",
+"baseGrossAmount":
+"item_baseGrossAmount",
+"baseNetAmount":
+"item_baseNetAmount",
+"baseNetDiscountAmount":
+"item_baseNetDiscountAmount",
+"shortName":
+"item_shortName",
+"categoryName":
+"item_categoryName",
+"createdTime":
+"item_createdTime"
+}
+
+for source_col, target_col in (
+column_mapping.items()
+):
+
+```
+# ---------------- CURRENT ---------------- #
+
+if (
+    source_col in current_sales.columns
+):
+
+    if (
+        target_col
+        not in current_sales.columns
+    ):
+
+        current_sales[
+            target_col
+        ] = current_sales[
+            source_col
+        ]
+
+    else:
+
+        current_sales[
+            target_col
+        ] = np.where(
+            (
+                current_sales[
+                    target_col
+                ].isna()
+            )
+            |
+            (
+                current_sales[
+                    target_col
+                ] == 0
+            )
+            |
+            (
+                current_sales[
+                    target_col
+                ] == ""
+            ),
+            current_sales[
+                source_col
+            ],
+            current_sales[
+                target_col
+            ]
+        )
+
+# ---------------- LW ---------------- #
+
+if (
+    source_col in lw_sales.columns
+):
+
+    if (
+        target_col
+        not in lw_sales.columns
+    ):
+
+        lw_sales[
+            target_col
+        ] = lw_sales[
+            source_col
+        ]
+
+    else:
+
+        lw_sales[
+            target_col
+        ] = np.where(
+            (
+                lw_sales[
+                    target_col
+                ].isna()
+            )
+            |
+            (
+                lw_sales[
+                    target_col
+                ] == 0
+            )
+            |
+            (
+                lw_sales[
+                    target_col
+                ] == ""
+            ),
+            lw_sales[
+                source_col
+            ],
+            lw_sales[
+                target_col
+            ]
+        )
+```
+
+print("✅ Item Metric Values Fixed")
+
+print(
+current_sales[
+[
+"item_quantity",
+"item_baseGrossAmount",
+"item_baseNetAmount",
+"item_baseNetDiscountAmount"
+]
+].head(10)
+)
+
+
+# =========================================================
 # EMPTY CHECK
 # =========================================================
 
